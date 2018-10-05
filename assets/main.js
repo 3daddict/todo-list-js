52a0ec66d98afe78b475a8075b7ba3c6871c13e7
3DAddict <print@3daddict.com>
2018-10-04 22:41:33 -0700

    working on style and layout

--- assets/main.js
+++ assets/main.js
@@ -4,51 +4,62 @@ let listArray = [];
 let listID = 0;
 let editID = 0;
 
-
 function todoAppLoaded() {
     console.log('ToDo App Ready');
-    //  $('#submitButton').click(addItem);
+
+    $('.button').on('click', function() {
+		$('.sidebar').toggleClass('isClosed');
+		$('.sidebar ul.nav').toggleClass('isClosed');
+	});
+
     $('#submitButton').click(inputValidation);
     keyValidation();
-    $(function() {
-        $( ".main-container" ).sortable();
+    $("#todoInput").keypress(errorMsg);
+
+    $(function () {
+        $(".main-container").sortable();
     });
 
     deleteListItem();
     editListItem();
 }
 
-function keyValidation(){
-    $('#todoInput').keypress(function (e) {
+function keyValidation() {
+    $('#todoInput').keypress(function (e) {  
         if (e.which == 13) {
-            console.log('Event: ' + e);
             inputValidation();
-          return false;    //<---- Add this line
+            return false;
         }
-      });
+    });
+}
+
+function errorMsg(){
+    let inputValue = $('#todoInput')
+    if(inputValue != ''){
+        $('#errorMsg').text('');
+    }
 }
 
 function inputValidation() {
     var empty = true;
     $('#todoInput[type="text"]').each(function () {
         if ($(this).val() == "") {
-            console.log('Please enter input');
+            $('#errorMsg').text('Error please enter an item in the form');
             empty = false;
             return false;
         } else {
+            $('#errorMsg').text('');
             addItem();
             empty = true
             return true;
         }
     });
-
 }
 
 function addItem() {
     let todoInput = $('#todoInput');
     let listItem = todoInput.val();
     let mainContainer = $('.main-container')
-
     let listCard = $('<div>').addClass('list-card sortable').attr('id', listID++);
 
     let listContainer = $('<div>').addClass('list-container');
@@ -62,73 +73,61 @@ function addItem() {
     let editInput = $('<input>').addClass('edit-input');
     let editButton = $('<button>').addClass('editBtn').attr('type', 'button')
     let editFontIcon = $('<i class="far fa-hand-pointer"></i>')
-    editButton.append(editFontIcon);
 
+    editButton.append(editFontIcon);
     mainContainer.append(listCard);
     listCard.append(listContainer, editContainer);
     listContainer.append(listDragIcon, listText, listEditIcon, listDeleteIcon);
     editContainer.append(editSpan, editInput, editButton);
-    
     todoInput.val("");
 
     listArray.push({
         id: listArray.length,
         item: listItem
     });
-    console.log(listArray);
 }
 
-function deleteListItem(){
-$('body').on('click', '.list-card .fa-ban', function () {
-    let listCardID = $(this).closest(".list-card").prop("id");
-    //let listCardText = $(this).parent().parent().text();
+function deleteListItem() {
+    $('body').on('click', '.list-card .fa-ban', function () {
+        let listCardID = $(this).closest(".list-card").prop("id");
 
-    //Loop through array and use this id to slice out this index
-    for(let i = 0; i < listArray.length; i++) {
-    if(listArray[i].id == listCardID) {
-        $(this).parent().parent().remove()
-        listArray.splice(i, 1);
-        console.log(listArray); 
-        break;
-    }
-}
-    
-});
+        //Loop through array and use this id to slice out this index
+        for (let i = 0; i < listArray.length; i++) {
+            if (listArray[i].id == listCardID) {
+                $(this).parent().parent().remove()
+                listArray.splice(i, 1);
+                break;
+            }
+        }
+    });
 }
 
-function editListItem(){
+function editListItem() {
     $('body').on('click', '.list-card .fa-edit', function () {
-        //let listCardID = $(this).closest(".list-card").prop("id");
         let listContainer = $(this).closest(".list-container");
 
         $(this).parent().parent().find('.edit-container').removeClass('hiddenInput');
         listContainer.addClass('hiddenInput');
-        
+
         editSubmitButton(listContainer);
     });
 }
 
-function editSubmitButton(listContainer){
+function editSubmitButton(listContainer) {
     $('body').on('click', '.list-card .editBtn', function () {
-    console.log('Submit button clicked');
-    let listCardID = $(this).closest(".list-card").prop("id");
-    let inputText = $(this).parent().find('.edit-input').val();
-    let updatedText = $(this).parent().parent().find('.list-text');
+        let listCardID = $(this).closest(".list-card").prop("id");
+        let inputText = $(this).parent().find('.edit-input').val();
+        let updatedText = $(this).parent().parent().find('.list-text');
+        updatedText.text(inputText);
 
-    // console.log($(this).parent().find('.edit-input').val());
-    console.log('ID: ' + listCardID);
-    console.log('Updated Text: ' + inputText);
-    updatedText.text(inputText);
+        //finds the index through this id and updates the array object
+        $.each(listArray, function () {
+            if (this.id == listCardID) {
+                this.item = inputText;
+            }
+        });
 
-//finds the index through this id and updates the array object
-    $.each(listArray, function() {
-        if (this.id == listCardID) {
-            this.item = inputText;
-        }
-    });
-
-    listContainer.removeClass('hiddenInput');
-    $(this).parent().parent().find('.edit-container').addClass('hiddenInput');
-    
+        listContainer.removeClass('hiddenInput');
+        $(this).parent().parent().find('.edit-container').addClass('hiddenInput');
     });
 }
